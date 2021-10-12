34c55ca33b9ab8e63b81a9a51c14427c1f7b89c1
imuWZG <90229130+imuWZG@users.noreply.github.com>
2021-10-12 09:02:58 +0800

    Add files via upload

---
+++ 网站/Home.js
@@ -0,0 +1,34 @@
+new Vue({
+    el: '#total',
+    data: {
+        WIN_width: (document.body.clientWidth) + 'px',
+        WIN_height: (document.body.clientHeight) + 'px',
+        picture_width: (document.body.clientWidth * 0.7) + 'px',
+        picture_height: (document.body.clientWidth * 0.3) + 'px',
+
+        picture1_width: (document.body.clientWidth * 0.08) + 'px',
+        picture1_height: (document.body.clientWidth * 0.08) + 'px',
+        picture2_width: (document.body.clientWidth * 0.24) + 'px',
+        picture2_height: (document.body.clientWidth * 0.08) + 'px',
+
+        scroll_height:(document.body.clientWidth*0.4) + 'px',
+    },
+    methods: {
+        changx: function (x) {
+            for (i = 1; i <= 10; i++) {
+                var b = document.getElementById("b" + i);
+                var d = document.getElementById("d" + i);
+                if (x == i) {
+                    d.style.display = 'block'
+                    b.style.backgroundColor = 'rgb(42, 75, 182)'
+                    b.style.color='rgb(255,255,255)'
+                }
+                else {
+                    d.style.display = 'none'
+                    b.style.backgroundColor = 'rgb(230, 230, 230)'
+                    b.style.color='rgb(42, 75, 182)'
+                }
+            }
+        }
+    }
+});
